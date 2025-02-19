b7a7620d69f08de768d01e4be5e3c56cb5f5f15c
JimKennedy <jim@midokura.com>
2025-02-19 13:26:17 +0100

    path

--- gistfile1.php
+++ gistfile1.php
@@ -1,8 +1,9 @@
 <?php
 //This will get an array of all the gif, jpg and png images in a folder
-$img_array = glob("/path/to/images/*.{gif,jpg,png}",GLOB_BRACE);
+$img_array = glob("/img/2024/*.{gif,jpg,png}",GLOB_BRACE);
 //Pick a random image from the array
 $img = array_rand($img_array);
 //Display the image on the page
 echo '<img alt="'.$img_array[$img].'" src="'.$img_array[$img].'" />';
 ?> 
+img/2024
